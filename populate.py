f8c20346d4fa0af2215611757378bc9ccc88fdf6
Noah Camara <noahcmr@gmail.com>
2022-12-23 12:22:52 -0500

    Updated get_early_clock_ins() to be able to also get late clock-outs

--- populate.py
+++ populate.py
@@ -4,22 +4,27 @@ weekly_timesheet = 'apr-weekly-timesheet-dec-5th.xlsx'
 ws = load_workbook(weekly_timesheet)['Sheet']
 
 
-def get_early_clock_ins():
-    """returns a list of dicts with early clock-in names, dates and times"""
-    early_clock_ins = []
-    for cell in ws['C']:
+def get_punch_times(column, am_pm) -> list:
+    """Returns a list of dicts with early clock in or late clock out names, dates and times. Example uses:
+    get_punch_times('E', 'pm'), get_punch_times('C', 'am')"""
+    punch_times = []
+    for cell in ws[column]:
         value = cell.value
         if not bool(value):
             continue
-        # Second part of bool makes sure number to left of colon is less than 8
-        if 'am' in value and int(value[0:value.index(':')]) < 8:
-            early_clock_ins.append({
-                'name': ws[f'A{cell.row}'].value,
-                'date': ws[f'B{cell.row}'].value,
-                'clock-in': value
-            })
-    return early_clock_ins
+        if am_pm in value:
+            left_of_colon = int(value[0:value.index(':')])
+            if am_pm == 'pm':
+                condition = left_of_colon > 5 and left_of_colon != 12
+            else:
+                condition = left_of_colon < 8
+            if condition:
+                punch_times.append({
+                    'name': ws[f'A{cell.row}'].value,
+                    'date': ws[f'B{cell.row}'].value,
+                    'clock-out': value
+                })
+    return punch_times
 
 
-print(get_early_clock_ins())
 # TODO: populate the template with early clock-in data
