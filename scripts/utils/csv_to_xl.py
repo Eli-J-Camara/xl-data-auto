365185e4c594b658fe53ea916159662f42093ea7
Noah Camara <noahcmr@gmail.com>
2023-01-18 10:51:00 -0500

    Fixed the UnicodeDecodeError

--- scripts/utils/csv_to_xl.py
+++ scripts/utils/csv_to_xl.py
@@ -1,10 +1,15 @@
-# source: https://www.blog.pythonlibrary.org/2021/09/25/converting-csv-to-excel-with-python/#:~:text=Converting%20a
-# %20CSV%20file%20to%20Excel,-You%20will%20soon&text=Your%20code%20uses%20Python's%20csv,to%20the%20input%20CSV%20file
+"""source: https://www.blog.pythonlibrary.org/2021/09/25/converting-csv-to-excel-with-python/#:~:text=Converting%20a
+%20CSV%20file%20to%20Excel,-You%20will%20soon&text=Your%20code%20uses%20Python's%20csv,to%20the%20input%20CSV%20file"""
 import csv
+import os
 import openpyxl
 
 
 def csv_to_xl(csv_file, excel_file):
+    # Avoids UnicodeDecodeError
+    for file in os.listdir():
+        if file == excel_file:
+            return
     csv_data = []
     with open(csv_file) as file_obj:
         reader = csv.reader(file_obj)
@@ -16,4 +21,3 @@ def csv_to_xl(csv_file, excel_file):
     for row in csv_data:
         sheet.append(row)
     workbook.save(excel_file)
- 
